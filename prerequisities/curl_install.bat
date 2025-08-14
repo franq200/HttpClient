7b495b372c8a583671a0ade9ca29f23cd6132f8d
franq200 <kuj-fran@wp.pl>
2025-08-14 19:44:31 +0200

    used vcpkg instead of packages

--- prerequisities/curl_install.bat
+++ prerequisities/curl_install.bat
@@ -4,4 +4,7 @@ git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 call bootstrap-vcpkg.bat
 .\vcpkg.exe install curl
+.\vcpkg.exe install nlohmann-json
+.\vcpkg.exe install gtest
+.\vcpkg.exe install gmock
 .\vcpkg.exe integrate install
